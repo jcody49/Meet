ed2e50fbcbdc283cf8a3e9423fff4022efa29d59
jcody49 <123919470+jcody49@users.noreply.github.com>
2023-08-23 19:59:24 -0600

    -
    -

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,18 +1,40 @@
 'use strict';
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const { google } = require("googleapis");//package needed from Google
+const calendar = google.calendar("v3");
+const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];//sets access level--READ only
+const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;//value being referred to is in the config.json file--keeps api values hidden
+const redirect_uris = [
+  "https://jcody49.github.io/meet/"
+];
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+
+//creates a new instance of the google.auth.OAuth2 method to be called
+const oAuth2Client = new google.auth.OAuth2(
+  CLIENT_ID,//accepts this as a param
+  CLIENT_SECRET,//accepts this as a param
+  redirect_uris[0]//accepts the exact uri within the const redirect_uris array above, which is number 0
+);
+
+module.exports.getAuthURL = async () => {//uses the module, Node.js module.exports to create the logic
+  /**
+   *
+   * Scopes array is passed to the `scope` option. 
+   *
+   */
+  const authUrl = oAuth2Client.generateAuthUrl({//allows you to seamlessly retrieve an access token, refresh it, and retry the request
+    access_type: "offline",
+    scope: SCOPES,
+  });
+
+  return {//contains the statusCode, headers, and body.
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify({
+      authUrl,
+    }),
+  };
 };
