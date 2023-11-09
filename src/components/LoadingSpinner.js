27229598b9e239fadd7c67942a7de1b1ceb038b6
jcody49 <123919470+jcody49@users.noreply.github.com>
2023-11-09 09:14:32 -0700

    adds loading wheel

---
+++ src/components/LoadingSpinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LoadingSpinner = () => {
+  return (
+    <div className="loading-spinner">
+      <div className="spinner"></div>
+      <div>Loading...</div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
