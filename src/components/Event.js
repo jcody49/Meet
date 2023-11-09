d963c11d54b59ddd367f84685573d89df58b7623
jcody49 <123919470+jcody49@users.noreply.github.com>
2023-11-09 10:30:28 -0700

    converts time and date into user friendly format
    also adds the time in show details dialog

--- src/components/Event.js
+++ src/components/Event.js
@@ -17,14 +17,20 @@ const Event = ({ event }) => {
     setShowDetails(!showDetails);
   };
   
+  // Format the date and time
+  const formattedDateTime = new Date(event.start.dateTime).toLocaleString();
+  
   return (
     // List item for the event
     <li>
       <div className="event">
         <h2 style={{ color: "#1e847f" }}>{event.summary}</h2>
         <div className="location">{event.location}</div>
-        <div className="dateTime">{event.start.dateTime}</div>
-        {showDetails && <div className="details">{event.description}</div>}
+
+        {showDetails && <div className="details">{event.description}
+        <br />
+        <strong>Date and Time:</strong> {formattedDateTime}
+        </div>}
         <button className="details-btn" onClick={toggleDetails}>
           {showDetails ? "Hide Details" : "Show Details"}
         </button>
